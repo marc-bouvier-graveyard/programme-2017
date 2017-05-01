0c0ecd5decf6dd4ee6b8ce28348b244b524d80aa
marc-bouvier <m.bouvier.dev@gmail.com>
2017-05-01 20:24:50 +0200

    filtrage par tags

--- data_2017.js
+++ data_2017.js
@@ -1,17 +1,118 @@
- var data_2017 = 
- {
-	"table" : ["Mesure","Description","Tenu","Source"],
-	"data":
-	[
-		{
-		"anchor"  : "travail",
-		"titre"   : "Travail",
-		"mesures" : [{"anchor"      : "pouvoir-achat",
-					  "title"       : "Nous améliorerons le pouvoir d’achat de tous les travailleurs.",
-					  "description" : "Sans que cela ne revienne plus cher aux employeurs, nous réduirons les cotisations payées par les salariés, par les indépendants et par les fonctionnaires: près de 500 euros supplémentaires nets par an pour un salaire de 2200 euros nets par mois!",
-					  "tenu"        : false,
-					  "source_tenu" : ""}
-					]
-		}
-	]
-};
+ var data_2017 = {
+     "table": ["Mesure", "Description", "Tenu", "Source", "Tags"],
+     "data": [{
+             "anchor": "travail",
+             "titre": "Travail",
+             "mesures": [{
+                     "anchor": "pouvoir-achat",
+                     "title": "Nous améliorerons le pouvoir d’achat de tous les travailleurs.",
+                     "description": "Sans que cela ne revienne plus cher aux employeurs, nous réduirons les cotisations payées par les salariés, par les indépendants et par les fonctionnaires: près de 500 euros supplémentaires nets par an pour un salaire de 2200 euros nets par mois!",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "pouvoir-achat-pauvres",
+                     "title": " Nous augmenterons le pouvoir d’achat des ouvriers, des employés et des salariés les moins bien payés. ",
+                     "description": "Tous les smicards qui bénéficient de la prime d’activité toucheront par exemple l’équivalent d’un 13e mois de salaire, soit 100€ nets de plus chaque mois.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": ["pouvoir d'achat", "smic", "prime d'activité"]
+                 },
+                 {
+                     "anchor": "exonerations-cotisations-sociales",
+                     "title": "Nous rétablirons les exonérations de cotisations sociales sur les heures supplémentaires.",
+                     "description": "",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "chomage-si-demission",
+                     "title": "Nous ouvrirons les droits à l’assurance-chômage aux salariés qui démissionnent.",
+                     "description": "Ce droit ne sera utilisable qu’une fois tous les cinq ans. En contrepartie, l’insuffisance des efforts de recherche d’emploi ou le refus d’offres raisonnables entraîneront la suspension des allocations.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "formation",
+                     "title": " Nous engagerons un effort national de formation sans précédent.",
+                     "description": "Nous formerons 1 million de jeunes et 1 million de demandeurs d’emploi peu ou pas qualifiés et aujourd’hui sans perspective.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "aide-agriculteurs",
+                     "title": "Nous serons aux côtés des agriculteurs pour qu’ils vivent de leur travail, plutôt que des aides publiques.",
+                     "description": "5 milliards d’euros de notre Plan d’investissement seront consacrés à la modernisation de leurs exploitations. Et nous les aiderons à être payés au prix juste en soutenant les organisations de producteurs dans leurs négociations avec les industriels. Nous nous battrons pour faire évoluer dans ce sens le droit européen de la concurrence.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "apprentissage",
+                     "title": " Nous ferons un effort massif pour l’apprentissage.",
+                     "description": "Nous développerons les périodes de pré-apprentissage et les filières en alternance dans tous les lycées professionnels. Des licences professionnelles seront préparées sur trois ans et en alternance. Nous ferons converger les contrats existants et impliquerons pleinement les branches professionnelles dans la définition des programmes et l’organisation des formations.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 }
+             ]
+         },
+         {
+             "anchor": "entreprise",
+             "titre": "Entreprise",
+             "mesures": [{
+                     "anchor": "simplification-suppression-rsi",
+                     "title": "Nous simplifierons la vie des entrepreneurs.",
+                     "description": "Nous réduirons leurs charges et supprimerons le Régime Social des Indépendants (RSI) qui ne fonctionne pas. Nous doublerons les plafonds pour pouvoir bénéficier du régime fiscal de la microentreprise. Nous mettrons fin à toute forme de concurrence déloyale en permettant chaque année à tous les artisans et commerçants d’opter ou non, selon leurs besoins, pour le régime fiscal de la microentreprise.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "reduction-cout-travail",
+                     "title": "Nous réduirons le coût du travail.",
+                     "description": "Nous aiderons les entreprises à embaucher en baissant les cotisations sociales employeurs de 6 points en remplacement du CICE, et jusqu’à 10 points au niveau du SMIC: les employeurs économiseront près de 1800 euros par an et par salarié au SMIC, 2200 euros par an pour un salarié payé 3000 euros bruts par mois.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "bonus-malus-chomage",
+                     "title": "Nous combattrons la précarité en responsabilisant les employeurs.",
+                     "description": "Nous créerons un bonus-malus sur l’assurance-chômage. Les employeurs qui entretiennent la précarité en recourant exagérément aux contrats courts paieront plus de charges, ceux qui créent des emplois stables en paieront moins.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "simplification-droit-travail",
+                     "title": "Nous redéfinirons le dialogue social. ",
+                     "description": "Les principes fondamentaux (durée légale du temps de travail, égalité professionnelle, salaire minimum...) resteront dans la loi. Mais, par exemple, les horaires effectifs ou l’organisation du travail seront négociés au plus près du terrain. Ils seront définis par accord majoritaire ou par référendum d’entreprise sur la base d’un accord.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "chomage-pour-tous",
+                     "title": "Nous permettrons à tous les travailleurs d’avoir droit à l’assurance-chômage. ",
+                     "description": "Les artisans, les commerçants indépendants, les entrepreneurs, les professions libérales, et les agriculteurs disposeront, comme les salariés, de cette protection. ",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 },
+                 {
+                     "anchor": "site-aide-reglementation-entreprises",
+                     "title": "Nous créerons sur Internet un nouveau service d’explication de la réglementation pour les entreprises.",
+                     "description": "Un site Internet unique donnera à chaque TPE ou PME les obligations légales et conventionnelles qu’elle doit respecter en fonction de sa situation. Les réponses fournies en ligne seront valables face à l’administration.",
+                     "tenu": false,
+                     "source_tenu": "",
+                     "tags": []
+                 }
+             ]
+         }
+     ]
+ };
